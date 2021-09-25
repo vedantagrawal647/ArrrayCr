ff635bd4b88933fe682eb43b11f3cb49c53f0923
Vedant Agrawal <vedantagrawal647@gmail.com>
2021-09-25 20:13:29 +0530

    Initial commit

---
+++ src/Array.java
@@ -0,0 +1,10 @@
+public class Array {
+    public static void main(String[] args){
+        String[] days = new String[] {"mon","tue","wed","thu","fri","sat","sun"};
+        
+
+        for(int i =1;i<=days.length ;i++)
+            System.out.println (days[i]);
+
+    }
+}
